0522df924ef84d54f35c1cec00c585a5ce239e7a
mitour <love2082020@gmail.com>
2015-06-19 14:32:51 +0800

    Modified theme of test page.

--- app/assets/javascripts/alertify/test.js
+++ app/assets/javascripts/alertify/test.js
@@ -1,12 +1,20 @@
 score = 0;
 $(".success").on( 'click', function () {
 	alertify.success("答對了");
+	$(this).parent().parent().css("background-color", "#E8F5E9");
+	$(this).css("color", "#388E3C");
+	$(this).parent().addClass("correct");
 	score = score + 10;
 	return false;
 });
 
 $(".error").on( 'click', function () {
 	alertify.error("答錯了");
+	$(this).parent().parent().css("background-color", "#FFEBEE");
+	$(this).css("color", "#D32F2F");
+	$(this).parent().addClass("wrong");
+	$(this).parent().parent().find(".success").parent().addClass("correct");
+	$(this).parent().parent().find(".success").css("color", "#388E3C");
 	return false;
 });
 
